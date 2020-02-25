d6e012ee4b290ac0bbdfce700014ebe97d05eb80
Scott Halgrim <shalgrim@gmail.com>
2020-02-25 08:47:14 -0800

    tests pass

--- day07_1.py
+++ day07_1.py
@@ -1,18 +1,28 @@
 import re
 
 ABBA_PATT = re.compile(r'(.)(.)\2\1')
-BRACKETED_ABBA_PATT = re.compile(r'\[[^\]]*(.)(.)\2\1\]')
+BRACKETED_ABBA_PATT = re.compile(r'\[[^\]]*(.)(.)\2\1\.*?]')
 
 
 def has_abba_in_brackets(s):
-    return BRACKETED_ABBA_PATT.search(s) is not None
+    return matches_with_differing_groups(s, BRACKETED_ABBA_PATT)
 
 
 def has_abba(s):
-    return ABBA_PATT.search(s) is not None
+    return matches_with_differing_groups(s, ABBA_PATT)
+
+
+def matches_with_differing_groups(s, pattern):
+    match_groups = pattern.findall(s)
+    if match_groups:
+        for mg in match_groups:
+            if mg[0] != mg[1]:
+                return True
+    return False
 
 
 def supports_tls(s):
     if not has_abba_in_brackets(s) and has_abba(s):
         return True
     return False
+
--- test_day07.py
+++ test_day07.py
@@ -6,10 +6,14 @@ class TestDay07(TestCase):
     def testHasAbba(self):
         self.assertTrue(has_abba('abba[mnop]qrst'))
         self.assertTrue(has_abba('abcd[bddb]xyyx'))
+        self.assertFalse(has_abba('aaaa[qwer]tyui'))
 
     def testHasAbbaInBrackets(self):
         self.assertTrue(has_abba_in_brackets('abcd[bddb]xyyx'))
+        self.assertFalse(has_abba_in_brackets('aaaa[qwer]tyui'))
 
     def testSupportsTls(self):
         self.assertTrue(supports_tls('abba[mnop]qrst'))
         self.assertFalse(supports_tls('abcd[bddb]xyyx'))
+        self.assertFalse(supports_tls('aaaa[qwer]tyui'))
+        self.assertTrue(supports_tls('ioxxoj[asdfgh]zxcvbn'))
