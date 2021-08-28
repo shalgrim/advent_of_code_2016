66e55083863817399cf7ad65f3a60c0b98cb96d0
Scott Halgrim <shalgrim@gmail.com>
2021-08-28 12:34:18 -0700

    day 18_1; build_map test passes

--- day18_1.py
+++ day18_1.py
@@ -11,7 +11,22 @@ TRAP_STRINGS = [''.join(t) for t in TRAP_INDICATORS]
 
 def get_next_row(row):
     next_row = [TRAP] if row[1] == TRAP else [SAFE]
-    next_row += [TRAP if row[i - 1 : i + 2] in TRAP_STRINGS else SAFE for i in range(1, len(row) - 1)]
+    next_row += [
+        TRAP if row[i - 1 : i + 2] in TRAP_STRINGS else SAFE
+        for i in range(1, len(row) - 1)
+    ]
     next_row.append(TRAP if row[-2] == TRAP else SAFE)
 
     return ''.join(next_row)
+
+
+def build_map(row, num_rows):
+    map = [row]
+    while len(map) < num_rows:
+        map.append(get_next_row(map[-1]))
+
+    return map
+
+
+def num_safe_tiles(map):
+    return sum(row.count(SAFE) for row in map)
--- test_day18.py
+++ test_day18.py
@@ -1,4 +1,19 @@
-from day18_1 import get_next_row
+from day18_1 import build_map, get_next_row, num_safe_tiles
+
+EXAMPLE_1 = ['..^^.', '.^^^^', '^^..^']
+
+EXAMPLE_2 = [
+    '.^^.^.^^^^',
+    '^^^...^..^',
+    '^.^^.^.^^.',
+    '..^^...^^^',
+    '.^^^^.^^.^',
+    '^^..^.^^..',
+    '^^^^..^^^.',
+    '^..^^^^.^^',
+    '.^^^..^.^^',
+    '^^.^^^..^^',
+]
 
 
 def test_get_next_row():
@@ -6,18 +21,14 @@ def test_get_next_row():
     for i in range(len(example1) - 1):
         assert get_next_row(example1[i]) == example1[i + 1]
 
-    example2 = [
-        '.^^.^.^^^^',
-        '^^^...^..^',
-        '^.^^.^.^^.',
-        '..^^...^^^',
-        '.^^^^.^^.^',
-        '^^..^.^^..',
-        '^^^^..^^^.',
-        '^..^^^^.^^',
-        '.^^^..^.^^',
-        '^^.^^^..^^',
-    ]
-    for i in range(len(example2) - 1):
-        assert get_next_row(example2[i]) == example2[i + 1]
+    for i in range(len(EXAMPLE_2) - 1):
+        assert get_next_row(EXAMPLE_2[i]) == EXAMPLE_2[i + 1]
+
+
+def test_build_map():
+    map = build_map(EXAMPLE_1[0], len(EXAMPLE_1))
+    assert map == EXAMPLE_1
+    map = build_map(EXAMPLE_2[0], len(EXAMPLE_2))
+    assert map == EXAMPLE_2
+
 
