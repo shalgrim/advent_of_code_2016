1b4f99ba6a4a211db33f3c9a4db728fde34816aa
Scott Halgrim <shalgrim@gmail.com>
2021-09-28 22:42:46 -0700

    takes too long

--- day19_1.py
+++ day19_1.py
@@ -6,16 +6,24 @@ def find_next_elf_with_presents(presents, elf):
     return next_elf
 
 
+def calc_remaining_elves(presents):
+    return len([present for present in presents if present])
+
+
 def main(num_elves):
     presents = [1] * num_elves
 
     elf = 0
-    while len([present for present in presents if present]) > 1:
+    remaining_elves = calc_remaining_elves(presents)
+    while remaining_elves > 1:
+        print(f'{remaining_elves=}')
         if presents[elf]:
             next_elf_with_presents = find_next_elf_with_presents(presents, elf)
             presents[elf] += presents[next_elf_with_presents]
             presents[next_elf_with_presents] = 0
         elf = (elf + 1) % len(presents)
+        remaining_elves = calc_remaining_elves(presents)
+
     for i, p in enumerate(presents):
         if p:
             return i+1
