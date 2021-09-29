9d0b3accefa165a13fc32a9e3745025e0e1c0a05
Scott Halgrim <shalgrim@gmail.com>
2021-09-28 23:10:11 -0700

    day 19 part 1 success

--- day19_1.py
+++ day19_1.py
@@ -1,33 +1,41 @@
-def find_next_elf_with_presents(presents, elf):
-    next_elf = (elf + 1) % len(presents)
-    while not presents[next_elf]:
-        next_elf = (next_elf + 1) % len(presents)
+class Elf:
+    def __init__(self, index, right=None):
+        self.index = index
+        self.presents = 1
+        self.right = right
+        if self.right:
+            self.right.left = self
+        self.left = None
 
-    return next_elf
+    def __repr__(self):
+        s = f'Elf #{self.index}; presents: {self.presents}'
+        s += f'; left: {self.left.index}' if self.left else '; left: None'
+        s += f'; right: {self.right.index}' if self.right else '; right: None'
+        return s
 
-
-def calc_remaining_elves(presents):
-    return len([present for present in presents if present])
+    def steal_presents(self):
+        self.presents += self.left.presents
+        self.left = self.left.left
+        self.left.right = self
 
 
 def main(num_elves):
-    presents = [1] * num_elves
+    first_elf = Elf(1)
+    prev_elf = first_elf
+    for i in range(2, num_elves):
+        elf = Elf(i, prev_elf)
+        prev_elf = elf
+    last_elf = Elf(num_elves, prev_elf)
+    last_elf.left = first_elf
+    first_elf.right = last_elf
 
-    elf = 0
-    remaining_elves = calc_remaining_elves(presents)
-    while remaining_elves > 1:
-        print(f'{remaining_elves=}')
-        if presents[elf]:
-            next_elf_with_presents = find_next_elf_with_presents(presents, elf)
-            presents[elf] += presents[next_elf_with_presents]
-            presents[next_elf_with_presents] = 0
-        elf = (elf + 1) % len(presents)
-        remaining_elves = calc_remaining_elves(presents)
+    current_elf = first_elf
+    while current_elf.left is not current_elf:
+        current_elf.steal_presents()
+        current_elf = current_elf.left
 
-    for i, p in enumerate(presents):
-        if p:
-            return i+1
+    return current_elf.index
 
 
 if __name__ == '__main__':
-    print(main(3005290))
+    print(main(3_005_290))
