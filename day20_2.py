15570dc097d793668ef2ba60e2b9db1e55cfbfea
Scott Halgrim <scott.halgrim@zapier.com>
2025-09-28 17:12:48 -0700

    wut

--- day20_2.py
+++ day20_2.py
@@ -1,38 +1,16 @@
-def split_line(line):
-    s1, s2 = line.split("-")
-    return int(s1), int(s2)
-
-
-def get_ranges(lines):
-    ranges = [split_line(line) for line in lines]
-    range_dict = {r[0]: r[1] for r in ranges}
-    return range_dict
-
-
-def find_a_lowest(range_dict, start):
-    verified = False
-    answer = start + 1
-    while not verified:
-        while answer in range_dict:
-            answer = range_dict[answer] + 1
-        for k, v in range_dict.items():
-            if k <= answer <= v:
-                answer = v + 1
-                break
-        else:
-            verified = True
-    return answer
+from day20_1 import get_ranges, find_a_lowest
 
 
 def main(lines):
     range_dict = get_ranges(lines)
-    highest_possible = 4_294_967_295
+    highest_possible = 4_294_967_295  # max possible IP address?
     known_blocked = -1
     answer = 0
-    while True:  # TODO: terminating condition has something to do with highest_possible
+    while known_blocked < highest_possible:
         lowest = find_a_lowest(range_dict, known_blocked)
         if lowest > highest_possible:
             break
+        answer = lowest
         # NEXT: Now that I've found a lowest unblocked
         # Find the next lowest blocked one, which should be easy
         # Then do some subtraction
